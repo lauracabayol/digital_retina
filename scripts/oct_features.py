296f2267e3dd8570c6ec418992961303f79840dc
Laura Cabayol <lauracabayol@gmail.com>
2023-03-24 12:00:50 +0100

    add training autoencoder

--- scripts/oct_features.py
+++ scripts/oct_features.py
@@ -1,9 +1,15 @@
 from sklearn.decomposition import PCA
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from autoencoder import MDNemulator_polyfit
 import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+import pytorch_lightning as pl
+from pytorch_lightning import Trainer
+from torch.utils.data import DataLoader, dataset, TensorDataset
+import torch
+
 
 class extract_features():
     def __init__(self, archive):
@@ -17,13 +23,23 @@ class extract_features():
         self.pca_comp = pca_comp
         return pca_comp
     
-    def _LDA_features(self,atr):
-        lda = LDA()
-        lda_comp = lda.fit_transform(getattr(self.archive, atr))
-        print('2 LDA components explain {lda.explained_variance_ratio_.sum()} of the variance')
+    def _autoencoder_features(self,ncomp,atr,lr=1e-3, epochs=100):
+        X_dat = getattr(self.archive, atr)
+        input_size = X_dat.shape[1]
         
-        self.lda_comp = lda_comp
-        return lda_comp
+        trainig_dataset = TensorDataset(torch.Tensor(X_dat))
+        loader_train = DataLoader(trainig_dataset, batch_size=100, shuffle = True)
+        
+        autoencoder = MDNemulator_polyfit(learning_rate=lr,epochs=epochs,ncomp=ncomp, input_size=input_size)
+        trainer = Trainer(max_epochs=epochs,enable_progress_bar=False)
+
+        trainer.fit(autoencoder, train_dataloaders=loader_train)
+
+        X_dat_latent,X_dat_out = autoencoder(torch.Tensor(X_dat))
+        
+        encoder_comp = X_dat_latent.detach().cpu().numpy()
+        self.encoder_comp=encoder_comp
+        return X_dat_out.detach().cpu().numpy(), encoder_comp
     
     
         
